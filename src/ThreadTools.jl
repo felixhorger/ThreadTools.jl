d715b3bb572ee464b70be2692b93cd6602b6bba4
Felix Horger <felix.horger@kcl.ac.uk>
2023-01-20 11:32:54 +0000

    Added protected indices feature. Made typestable

--- src/ThreadTools.jl
+++ src/ThreadTools.jl
@@ -2,18 +2,35 @@
 module ThreadTools
 	
 	export safe_split_threads
-	
+
 	"""
-		idx must be sorted
+		no need to sort idx
 	"""
-	function safe_split_threads(idx::AbstractVector{CartesianIndex{N}}, nthreads::Integer) where N
+	function safe_split_threads(
+		idx::AbstractVector{CartesianIndex{N}},
+		protect::NTuple{M, Integer},
+		nthreads::Integer
+	) where {N, M}
+		# Sort by protected indices
+		idx = sort(idx; by = i -> CartesianIndex{M}((i[l] for l in protect)...))
 		# Find block safe to process only sequentially
 		k = idx[1]
 		blocks = Vector{Int}(undef, 0)
 		sizehint!(blocks, length(idx))
 		n = 1
 		for (i, j) in enumerate(idx)
-			k == j && continue
+			# Continue if protected indices are equal
+			# Note: this is practically C-code because the type inference of julia fails if a
+			# much nicer generator-one-liner is used ...
+			equal = true
+			for l in protect
+				if k[l] != j[l]
+					equal = false
+					break
+				end
+			end
+			equal && continue
+			# Indices differ, start new block
 			push!(blocks, n)
 			k = j
 			n = i
@@ -21,13 +38,15 @@ module ThreadTools
 		push!(blocks, length(idx)+1)
 		block_lengths = diff(blocks)
 		# Find a split which is kind of even
-		split_idx = ntuple(_ -> Vector{CartesianIndex{N}}(undef, 0), nthreads)
+		split_idx = [Vector{CartesianIndex{N}}(undef, 0) for t = 1:nthreads]
 		sizehint!.(split_idx, length(idx) ÷ nthreads + 1)
+		lengths = Vector{Int}(undef, nthreads)
 		for p in sortperm(block_lengths; rev=true)
-			t = argmin(length.(split_idx))
+			lengths .= length.(split_idx)
+			t = argmin(lengths)
 			@views append!(split_idx[t], idx[blocks[p]:blocks[p+1]-1])
 		end
-		return split_idx
+		return sort.(split_idx)
 	end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,9 +2,19 @@
 using Revise
 using ThreadTools
 
-idx = CartesianIndex.(mod1.(rand(Int, 20), 1000))
-sort!(idx)
-splits, block_lengths = safe_split_threads(idx, 4)
+idx = mod1.(rand(Int, 3, 1000000), 5)
+idx = [CartesianIndex(idx[:, i]...) for i in axes(idx, 2)]
+
+@time splits = safe_split_threads(idx, (1, 3), 4);
+
+
+i = splits[1][1]
+for t = 2:4
+	for j in splits[t]
+		@assert j[1] != i[1] || j[3] != i[3]
+	end
+end
+
 length.(splits)
 
 
