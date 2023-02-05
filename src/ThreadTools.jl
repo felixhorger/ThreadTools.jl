0d204eb31696746ef00b1abeda962c20f26902b9
Felix Horger <felix.horger@kcl.ac.uk>
2023-02-05 21:52:34 +0000

    Improved safe_split...() and added @threads to allow a prep function in each thread

--- src/ThreadTools.jl
+++ src/ThreadTools.jl
@@ -30,42 +30,65 @@ module ThreadTools
 		protect::NTuple{M, Integer},
 		nthreads::Integer
 	) where {N, M}
-		# Sort by protected indices
-		idx = sort(idx; by = i -> CartesianIndex{M}((i[l] for l in protect)...))
-		# Find block safe to process only sequentially
-		k = idx[1]
-		blocks = Vector{Int}(undef, 0)
-		sizehint!(blocks, length(idx))
-		n = 1
-		for (i, j) in enumerate(idx)
-			# Continue if protected indices are equal
-			# Note: this is practically C-code because the type inference of julia fails if a
-			# much nicer generator-one-liner is used ...
-			equal = true
-			for l in protect
-				if k[l] != j[l]
-					equal = false
-					break
+		@inbounds begin
+			@assert all(p -> 1 ≤ p ≤ N, protect)
+			# Sort by protected indices
+			idx = sort(idx; by = i -> CartesianIndex{M}((i[l] for l in protect)...))
+			# Find block safe to process only sequentially
+			k = idx[1]
+			blocks = Vector{Int}(undef, 0)
+			sizehint!(blocks, length(idx))
+			n = 1
+			for (i, j) in enumerate(idx)
+				# Continue if protected indices are equal
+				# Note: this is practically C-code because the type inference of julia fails if a
+				# much nicer generator-one-liner is used ...
+				equal = true
+				for l in protect
+					if k[l] != j[l]
+						equal = false
+						break
+					end
 				end
+				equal && continue
+				# Indices differ, start new block
+				push!(blocks, n)
+				k = j
+				n = i
 			end
-			equal && continue
-			# Indices differ, start new block
-			push!(blocks, n)
-			k = j
-			n = i
+			push!(blocks, length(idx)+1)
+			block_lengths = diff(blocks)
+			# Find a split which is kind of even
+			split_idx = [Vector{CartesianIndex{N}}(undef, 0) for t = 1:nthreads]
+			sizehint!.(split_idx, length(idx) ÷ nthreads + 1)
+			lengths = Vector{Int}(undef, nthreads)
+			for p in sortperm(block_lengths; rev=true)
+				lengths .= length.(split_idx)
+				t = argmin(lengths)
+				@views append!(split_idx[t], idx[blocks[p]:blocks[p+1]-1])
+			end
+			return split_idx
 		end
-		push!(blocks, length(idx)+1)
-		block_lengths = diff(blocks)
-		# Find a split which is kind of even
-		split_idx = [Vector{CartesianIndex{N}}(undef, 0) for t = 1:nthreads]
-		sizehint!.(split_idx, length(idx) ÷ nthreads + 1)
-		lengths = Vector{Int}(undef, nthreads)
-		for p in sortperm(block_lengths; rev=true)
-			lengths .= length.(split_idx)
-			t = argmin(lengths)
-			@views append!(split_idx[t], idx[blocks[p]:blocks[p+1]-1])
+	end
+
+	"""
+	"""
+	function thread_region(
+		tid::Integer,
+		niter::Integer,
+		nthreads::Integer
+	)
+		num = niter ÷ nthreads
+		start = num * (tid - 1)
+		rem = mod(niter, nthreads)
+		if tid ≤ rem
+			num += 1
+			start += tid
+		else
+			start += rem+1
 		end
-		return sort.(split_idx)
+		stop = start + num - 1
+		return start, stop
 	end
 
 
@@ -79,24 +102,16 @@ module ThreadTools
 		iter_var, iter = loop.args[1].args
 		body = loop.args[2]
 		return quote
-			let niter, nthreads, per_thread
+			let niter, nthreads
 				niter = length($(esc(iter)))
 				nthreads = min(Threads.nthreads(), niter)
-				per_thread = niter ÷ nthreads
 				$(macroexpand(Main, quote
 				@sync for tid = 1:nthreads
-					Threads.@spawn let per_thread=per_thread, start
-						start = per_thread * (tid - 1)
-						rem = mod(niter, nthreads)
-						if tid ≤ rem
-							per_thread += 1
-							start += tid-1
-						else
-							start += rem
-						end
+					Threads.@spawn let start, num
+						start, stop = thread_region(tid, niter, nthreads)
 						let $(esc.(prep_vars.args)...)
 							$(esc(prep_vars)) = $(esc(prep_func))()
-							for i = start+1 : start+per_thread
+							for i = start:stop
 								local $(esc(iter_var)) = @inbounds $iter[i]
 								$(esc(body))
 							end
